1ec662fd6a32f9822efdc077db5809b6e5bf2937
예언 <dpdjs0831@gmail.com>
2025-05-13 17:26:21 +0900

    Create gameobject.cs

---
+++ gameobject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class gameobject : MonoBehaviour
+{
+
+    void Start(){
+        gameobject.SetActive(false); //게임 오브젝트 비활성화
+        gameobject.SetActive(True); //게임 오브젝트 활성화
+    }
+}
